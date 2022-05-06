cbadebafa7008dba4195ed9164ebb25c6d526f6e
Thiago Vasconcelos <61916428+tvtfisica@users.noreply.github.com>
2022-05-06 13:07:33 -0300

    Update Blink.ino

--- Blink.ino
+++ Blink.ino
@@ -1,25 +1,23 @@
 /*
   Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO 
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino model, check
-  the Technical Specs of your board  at https://www.arduino.cc/en/Main/Products
+  Acende um LED por um segundo, depois apaga por um segundo, repetidamente.
+  A maioria dos Arduinos tem um LED integrado que você pode controlar. No UNO, MEGA e ZERO
+  ele está conectado ao pino digital 13, no MKR1000 no pino 6. LED_BUILTIN é definido como
+  o pino de LED correto, independentemente de qual placa é usada.
+  Se você quiser saber em qual pino o LED on-board está conectado no seu modelo Arduino, verifique
+  as especificações técnicas da sua placa em https://www.arduino.cc/en/Main/Products
   
-  This example code is in the public domain.
- 
+  Este código de exemplo é de domínio público.
 */
 
 
-// the setup function runs once when you press reset or power the board
-void setup() {
+// A função de configuração é executada uma vez quando você pressiona reset ou liga a placa
+void configuração() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(13, OUTPUT);
 }
 
-// the loop function runs over and over again forever
+// A função de loop é executada repetidamente para sempre
 void loop() {
   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
